025f896beb9c180be547a1c990da7c5a095dc177
TheAhmedHameed <48395433+TheAhmedHameed@users.noreply.github.com>
2020-10-14 11:41:00 -0400

    Update AppExecutors.java

--- PopularMovieApp1/app/src/main/java/com/example/popularmovieapp1/AppExecutors.java
+++ PopularMovieApp1/app/src/main/java/com/example/popularmovieapp1/AppExecutors.java
@@ -1,10 +1,7 @@
 package com.example.popularmovieapp1;
-
 import android.os.Handler;
 import android.os.Looper;
-
 import androidx.annotation.NonNull;
-
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
